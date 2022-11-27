e5b8842d34a207a19e60cbfead98ccdc5ddd1089
Agustin-Basilio-Matijasevich <agustinbasiliomatijasevich@gmail.com>
2022-11-27 20:34:36 -0300

    Update managers.py
    Pequeños cambios para adaptar correctamente al usuario

--- applications/user/managers.py
+++ applications/user/managers.py
@@ -2,10 +2,9 @@ from django.db import models
 from django.contrib.auth.models import BaseUserManager
 
 class ManagerUsuario(BaseUserManager, models.Manager):
-    def _create_user(self, username, nombres,password, is_staff, is_superuser, **extra_fields):
+    def _create_user(self, username, password, is_staff, is_superuser, **extra_fields):
         user = self.model(
             username = username,
-            nombres = nombres,
             is_staff = is_staff,
             is_superuser = is_superuser,
             **extra_fields
@@ -14,8 +13,8 @@ class ManagerUsuario(BaseUserManager, models.Manager):
         user.save(using=self.db)
         return user
 
-    def create_user(self, username, nombres, password=None, **extra_fields):
-        return self._create_user(username, nombres, password, False, False, **extra_fields)
+    def create_user(self, username, password=None, **extra_fields):
+        return self._create_user(username, password, False, False, **extra_fields)
 
-    def create_superuser(self, username,nombres, password=None, **extra_fields):
-        return self._create_user(username,nombres ,password, True, True, **extra_fields)
+    def create_superuser(self, username, password=None, **extra_fields):
+        return self._create_user(username, password, True, True, **extra_fields)
