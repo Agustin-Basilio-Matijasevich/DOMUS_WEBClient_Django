6776f67cedbc72cea82143587063284769bb8fc3
Agustin-Basilio-Matijasevich <agustinbasiliomatijasevich@gmail.com>
2022-11-28 00:02:24 -0300

    Update admin.py
    Se mejoro la personalizacion de la vista para administrar usuarios

--- applications/user/admin.py
+++ applications/user/admin.py
@@ -1,7 +1,20 @@
 from django.contrib import admin
 from .models import Usuario
 
+admin.site.site_header = 'DOMUS 2.0 Alpha'
+
+admin.site.index_title = 'Administracion'
+
+admin.site.site_title = 'DOMUS'
+
 class UserAdmin(admin.ModelAdmin):
+
+    list_display = ('username', 'nombres', 'tipo_usuario', 'is_superuser', 'is_staff')
+
+    search_fields =  ('username', 'nombres', 'apellidos', 'dni_cuil', 'email')
+
+    ordering = ('-is_superuser', '-is_staff', 'username',)
+
     def save_model(self, request, obj, form, change):
         if obj.password.startswith('pbkdf2'):
             obj.password=obj.password
