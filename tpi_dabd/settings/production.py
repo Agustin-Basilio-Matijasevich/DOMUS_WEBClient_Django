e21e0bb057b99861b60df136826ba35300833fef
Agustin-Basilio-Matijasevich <agustinbasiliomatijasevich@gmail.com>
2022-11-28 15:03:18 -0300

    Autorizacion del host

--- tpi_dabd/settings/production.py
+++ tpi_dabd/settings/production.py
@@ -3,7 +3,7 @@ import os
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['192.168.0.61', 'electronicanordeste.tplinkdns.com', 'localhost', '127.0.0.1']
 
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
