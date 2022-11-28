0674e33b7041a031a6309321bbfea2c6eccb0c61
Agustin-Basilio-Matijasevich <agustinbasiliomatijasevich@gmail.com>
2022-11-27 21:40:43 -0300

    Update admin.py
    Se añadio correccion que permite crear correctamente contraseñas desde la pagina de administracion Django

--- applications/user/admin.py
+++ applications/user/admin.py
@@ -1,4 +1,12 @@
 from django.contrib import admin
 from .models import Usuario
 
-admin.site.register(Usuario)
+class UserAdmin(admin.ModelAdmin):
+    def save_model(self, request, obj, form, change):
+        if obj.password.startswith('pbkdf2'):
+            obj.password=obj.password
+        else:
+            obj.set_password(obj.password) 
+        super().save_model(request, obj, form, change)
+
+admin.site.register(Usuario, UserAdmin)
